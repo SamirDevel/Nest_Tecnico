99d3bf2f0545fe0ec95bfeb3c91131ed6dd7404b
Samir Priego <samir.apriego@gmail.com>
2024-05-06 17:47:23 -0600

    Filtro correcto

--- src/utilities/filter.ts
+++ src/utilities/filter.ts
@@ -1,13 +1,14 @@
 export default function Filter<T>(array:T[], filters:T){
     const keys = Object.keys(filters)
     return array.filter(item=>{
-        keys.forEach(key=>{
+        for(const i in keys){
+            const key = keys[i];
             if(filters[key]!==undefined){
                 if(filters[key]!==item[key]){
                     return false
                 }
             }
-        })
+        }
         return true
     })
 }
